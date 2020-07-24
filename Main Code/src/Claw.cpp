528b3d10a090f7e227b7c10b2e3cce2a801f0129
sylviazhang9 <sylvia.zhang@alumni.ubc.ca>
2020-07-24 12:01:04 -0400

    changed Claw.cpp functions, removed position function arguments

--- Main Code/src/Claw.cpp
+++ Main Code/src/Claw.cpp
@@ -6,7 +6,8 @@ Claw::Claw(){
     NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
   }
 
-  void Claw::openClaw(int blueServoPos){
+  void Claw::openClaw(){
+	int blueServoPos = blueServo.read();
     for (int pos = blueServoPos; pos <= openPos; pos++){
       blueServo.write(pos);
       delay(5);
@@ -14,9 +15,10 @@ Claw::Claw(){
     return;
   }
 
-  void Claw::closeClaw(int blueServoPos){
-    if (blueServoPos!=openPos) {
-      openClaw(blueServoPos);
+  void Claw::closeClaw(){
+    int blueServoPos = blueServo.read();
+	if (blueServoPos!=openPos) {
+      openClaw();
     }
     for (int pos = openPos; pos >= closePos; pos--) {
       blueServo.write(pos);
@@ -25,15 +27,17 @@ Claw::Claw(){
     return;
   }
 
-  void Claw::lowerClaw(int greyServoPos) { 
-    for (int pos = greyServoPos; pos >= downPos; pos--) {
+  void Claw::lowerClaw() { 
+    int greyServoPos = greyServo.read();
+	for (int pos = greyServoPos; pos >= downPos; pos--) {
       greyServo.write(pos);
       delay(15);
     }
     return;
   }
 
-  void Claw::raiseClaw(int greyServoPos) {
+  void Claw::raiseClaw() {
+	int greyServoPos = greyServo.read();
     for (int pos = greyServoPos; pos <= upPos; pos++){
       greyServo.write(pos);
       delay(15);
