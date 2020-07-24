cbe1126a9bc8e47e2a403af3fac8920264630b31
sylviazhang9 <sylvia.zhang@alumni.ubc.ca>
2020-07-24 12:14:57 -0400

    changed Claw.h to include position values

--- Main Code/include/Claw.h
+++ Main Code/include/Claw.h
@@ -5,18 +5,18 @@
 #include "INIT.h"
 
 // For Sonar on Claw
-#define TRIGGER_PIN PB_5
-#define ECHO_PIN PB_4
+#define TRIGGER_PIN PB5
+#define ECHO_PIN PB4
 #define MAX_DISTANCE 320
 
 // For Claw Servo
 #define servoClawPin PA7
-#define clawMaxAngle 120
+#define clawMaxAngle 180
 #define clawSpeed 2
 
 // For Arm Servo
 #define servoArmPin PA8
-#define armMaxAngle 100
+#define armMaxAngle 120
 
 class Claw
 {
@@ -30,6 +30,11 @@ public:
 private:
     Servo blueServo;
     Servo greyServo;
+	
+	int openPos = (clawMaxAngle-1);
+	int closePos = 0;
+	int downPos = 0;
+	int upPos = 90;
 
     long distance = -1;
     int blueServoPos = -1;
