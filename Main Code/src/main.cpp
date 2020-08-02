5d84e4ea0a59f378cf2a50714c9d3650f58de8ba
farrandi <farrandi.nando@alumni.ubc.ca>
2020-08-02 13:45:01 -0700

    Skeleton code for main
    Signed-off-by: farrandi <farrandi.nando@alumni.ubc.ca>

--- Main Code/src/main.cpp
+++ Main Code/src/main.cpp
@@ -1,142 +1,100 @@
-#include "INIT.h"
+#include <INIT.h>
+#include <Motor.h>
+#include <Claw.h>
 
-void disp_setup();
-void disp_reset();
-
-#define ANALOG_MAX 1023
-#define MAX_I 200
-#define SPEED MAX_MOTOR/10*6
-
-#define ERROR_RANGE 10
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-volatile short int kp = 6;
-volatile short int kd = 0;
-volatile short int ki = 0;
-
-volatile short int P = 0;
-volatile short int D = 0;
-volatile short int I = 0;
-volatile short int G = 0;
-
-volatile short int err = 0;
-volatile short int prev = 0;
-
-volatile unsigned short int left;
-volatile unsigned short int right;
-volatile unsigned short int left_high;
-volatile unsigned short int left_low;
-volatile unsigned short int right_high;
-volatile unsigned short int right_low;
+//STATE CONSTANTS
+#define SEARCH 0 //default
+#define PICK_UP 1
+#define LOOK 2
+#define HOME 3
+#define DROP 4
+#define AVOID 5
+#define FUN 6
+#define OFF 8
+int state = 0;
+int prev_state = state;
 
 void setup()
 {
-  pinMode(IR_LA, INPUT);
-  pinMode(IR_LB, INPUT);
-  pinMode(IR_RA, INPUT);
-  pinMode(IR_RB, INPUT);
-  pinMode(A7, INPUT_PULLUP);
-
-  disp_setup();
-}
-
-void disp_setup()
-{
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-}
-
-void disp_reset()
-{
-  display.clearDisplay();
-  display.setCursor(0, 0);
 }
 
 void loop()
 {
-  disp_reset();
-  left_high = analogRead(IR_LA);
-  left_low = analogRead(IR_LB);
-  right_high = analogRead(IR_RA);
-  right_low = analogRead(IR_RB);
+  prev_state = state;
 
-  if (left_high > ANALOG_MAX)
+  switch (state)
   {
-    left = left_low;
-  }
-  else
-  {
-    left = left_high;
-  }
+  default:
 
-  if (right_high > ANALOG_MAX)
-  {
-    right = right_low;
-  }
-  else
-  {
-    right = right_high;
-  }
-
-  display.print("A1: ");
-  display.println(left_high);
-  display.print("A0: ");
-  display.println(left_low);
-
-  display.print("B1: ");
-  display.println(right_high);
-  display.print("B0: ");
-  display.println(right_low);
-
-  display.display();
-
-  //if (digitalRead(PA7) == HIGH)
-  //{
-    err = left - right;
-
-    if (err < ERROR_RANGE || err > -ERROR_RANGE)
+    if (sonar == 'Y')
     {
-      pwm_start(MOTOR_LF, FREQUENCY, SPEED, RESOLUTION_16B_COMPARE_FORMAT);
-      pwm_start(MOTOR_RF, FREQUENCY, SPEED , RESOLUTION_16B_COMPARE_FORMAT);
+      state = HEAD;
+      break;
     }
     else
     {
-      P = kp * err;
-      D = kd * (err - prev);
-      I = (ki * err) + I;
-      if (I > MAX_I)
-        I = MAX_I;
-      if (I < -MAX_I)
-        I = -MAX_I;
-      G = P + D + I;
-      if (err > 0)
+      break;
+    }
+
+  case HEAD:
+
+    if (can == 'N')
+    {
+      state = SEARCH;
+      break;
+    }
+    else
+    {
+      if (close == 'Y')
       {
-        pwm_start(MOTOR_LF, FREQUENCY, SPEED - G, RESOLUTION_16B_COMPARE_FORMAT);
-        pwm_start(MOTOR_RF, FREQUENCY, SPEED + G, RESOLUTION_16B_COMPARE_FORMAT);
+        state = PICK_UP;
+        break;
       }
       else
       {
-        pwm_start(MOTOR_LF, FREQUENCY, SPEED + G, RESOLUTION_16B_COMPARE_FORMAT);
-        pwm_start(MOTOR_RF, FREQUENCY, SPEED - G, RESOLUTION_16B_COMPARE_FORMAT);
+        break;
       }
-      prev = err;
-
-      display.print("Error: ");
-      display.println(err);
-      display.print("Gain: ");
-      display.println(G);
-
-      display.display();
     }
-  // }
-  // else
-  // {
-  //   display.println("OFF");
+  case PICK_UP:
 
-  //   display.display();
-  // }
+    if (can == 'N')
+    {
+      state = SEARCH;
+      break;
+    }
+    else
+    {
+
+      state = HOME;
+      break;
+    }
+  case HOME:
+
+    if (can == 'N')
+    {
+      state = SEARCH;
+      break;
+    }
+
+    if (bin == 'Y')
+    {
+      state = DROP;
+      break;
+    }
+    else
+    {
+      break;
+    }
+  case DROP:
+
+    state = SEARCH;
+    break;
+  case AVOID:
+
+    state = prev_state;
+    break;
+  case FUN:
+
+    break;
+  }
 }
