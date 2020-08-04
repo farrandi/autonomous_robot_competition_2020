cc105410415aab400c18d522993af3833360aa8f
thomasclarito <43707071+thomasclarito@users.noreply.github.com>
2020-08-04 16:23:58 -0600

    created the search code

--- Main Code/src/main.cpp
+++ Main Code/src/main.cpp
@@ -15,28 +15,45 @@
 int state = 0;
 int prev_state = state;
 
+//INITIALIZING COMPONENTS
 Display myDisp;
 Motor myMotor;
 Claw myClaw;
+Ultrasonic sonar(TRIGGER_PIN, ECHO_PIN, 15000UL);
+
+//INITIALIZING TIMER CONSTANTS
+const int sonarInterval = 20;             // the interval between sonar pings
+unsigned long currentMillis = 0;          // the value of millis in the current iteration of the loop
+unsigned long previousSonarMillis = 0;    // the previous valut of the sonar millis.
+int sonarReading = 0;                     // the sonarReading value in cm
+int sonarThreshold = 50;                  // the sonar threshold value for detecting objects
+const int clawRangeLB = 10;               // the claw range lower bound
+const int clawRangeUB = 14;               // the claw range upper bound
+
+bool ping();
+bool search();
 
 void setup() {
   myDisp.setup();
+  myClaw.setup();
 }
 
 void loop() {
   prev_state = state;
+  currentMillis = millis();
 
   switch (state)
   {
   default:
 
-    if (sonar == 'Y')
+    if (sonarFind == 'Y')     // If sonar finds object
     {
       state = HEAD;
       break;
     }
     else
     {
+      //add a drive forward?
       break;
     }
 
@@ -101,4 +118,34 @@ void loop() {
 
     break;
   }
+}
+
+
+bool ping() {
+  bool objectDetected = false;
+  if (currentMillis - previousSonarMillis >= sonarInterval) {
+    sonarReading = sonar.read();
+    previousSonarMillis += sonarInterval;
+    if (sonarReading <= sonarThreshold) {
+      return true;
+    } 
+  }
+  return false;
+}
+
+bool search() {
+  if (ping() == true) {
+    if (sonarReading >= clawRangeUB) {
+      myMotor.drive_forward(5);
+    } else if (sonarReading <= clawRangeLB) {
+      myMotor.drive_backward(5);
+    }
+    else if (sonarReading <= clawRangeUB && sonarReading >= clawRangeLB) {
+      myMotor.stop();
+      return true;
+    }
+  } else { 
+    myMotor.drive_cw();
+  }
+
 }
