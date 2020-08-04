ec4e77fb45abb692e982a1259f509ed980f9bde4
sylviazhang9 <sylvia.zhang@alumni.ubc.ca>
2020-08-03 22:58:38 -0400

    added new claw.h and claw.cpp files, and i think i changed INIT.h

--- Main Code/include/Claw.h
+++ Main Code/include/Claw.h
@@ -2,20 +2,39 @@
 #define Claw_h
 #endif
 
-#include "INIT.h"
+#include <INIT.h>
 
-class Claw
-{
-public:
-    Claw();
-    void setup();
-    void openClaw();
-    void closeClaw();
-    void lowerClaw();
-    void raiseClaw();
-    int readSmallServo();
-    int readbigServo();
-    void writeSmallServo(int pos);
-    void writeBigServo(int pos);
-    
-};
+// CONSTANTS
+#define SERVO_FREQ 50
+#define MIN_ANGLE_PULSE 500
+#define MAX_ANGLE_PULSE 2500
+
+const int closepos = CLOSEPOS;
+const int openpos = OPENPOS;
+const int speedsmall = SPEED_SMALL;
+const int downpos = DOWNPOS;
+const int uppos = UPPOS;
+const int speedbig = SPEED_BIG;
+
+Class Claw(){
+    public:
+        // Constructors
+        Claw();
+
+        // Sets up the claw
+        void setup();
+
+        // claw motions
+        void open();
+        void close();
+        void raise();
+        void lower();
+
+        // servo write
+        void writeSmall(int degree);
+        void writeBig(int degree);
+
+    private:
+        int convertSmall(int degree);
+        int convertBig(int degree);
+}
--- Main Code/src/Claw.cpp
+++ Main Code/src/Claw.cpp
@@ -1,69 +1,106 @@
 #include <Claw.h>
 
-Servo smallServo;
-Servo bigServo;
-
-Claw::Claw(){
-
-  }
+/* empty constructor
+ */
+Claw::Claw(){}
 
 void Claw::setup(){
-  smallServo.attach(SMALLSERVO_PIN);
-  bigServo.attach(BIGSERVO_PIN);
-  bigServo.write(DOWNPOS);
-  return;
+  pinMode(SMALLSERVO_PIN,OUTPUT);
+  pinMode(BIGSERVO_PIN,OUTPUT);
 }
 
-  void Claw::openClaw(){
-	int smallServoPos = smallServo.read();
-    for (int pos = smallServoPos; pos <= OPENPOS; pos++){
-      smallServo.write(pos);
-      delay(5);
+/* Opens the claw
+ */
+void Claw::open(){
+  if(closepos < openpos) {
+    for (int i = closepos; i <= openpos; i+=speedsmall){
+      writeSmall(i);
     }
-    return;
-  }
-
- void Claw::closeClaw(){
-  int smallServoPos = smallServo.read();
-	if (smallServoPos != OPENPOS) {
-      openClaw();
-    }
-    for (int pos = OPENPOS; pos >= CLOSEPOS; pos--) {
-      smallServo.write(pos);
-      delay(5);
-    }
-    return;
-  }
-
-void Claw::lowerClaw() { 
-    int bigServoPos = bigServo.read();
-	for (int pos = bigServoPos; pos >= DOWNPOS; pos--) {
-      bigServo.write(pos);
-      delay(15);
-    }
-    return;
-  }
-
-void Claw::raiseClaw() {
-	int bigServoPos = bigServo.read();
-    for (int pos = bigServoPos; pos <= UPPOS; pos++){
-      bigServo.write(pos);
-      delay(15);
+  } else{
+    for (int i = closepos; i >= openpos; i-=speedsmall){
+      writeSmall(i);
     }
   }
+}
 
-  int Claw::readSmallServo(){
-    return smallServo.read();
+/* Closes the claw
+ */
+void Claw::close(){
+  if(openpos > closepos) {
+    for (int i = openpos; i >= closepos; i-=speedsmall){
+      writeSmall(i);
+    }
+  } else{
+    for (int i = openpos; i <= closepos; i+=speedsmall){
+      writeSmall(i);
+    }
   }
+}
 
-  int Claw::readbigServo(){
-    return bigServo.read();
+/* Raises the claw
+ */
+void Claw::raise(){
+  if(downpos < uppos) {
+    for (int i = downpos; i <= uppos; i+=speedbig){
+      writeBig(i);
+    }
+  } else{
+    for (int i = downpos; i >= uppos; i-=speedbig){
+      writeBig(i);
+    }
   }
+}
 
-  void Claw::writeSmallServo(int pos){
-    smallServo.write(pos);
+/* Lowers the claw
+ */
+void Claw::lower(){
+  if(uppos > downpos) {
+    for (int i = uppos; i >= downpos; i-=speedbig){
+      writeBig(i);
+    }
+  } else{
+    for (int i = uppos; i <= downpos; i+=speedbig){
+      writeBig(i);
+    }
   }
+}
 
-  void Claw::writeBigServo(int pos){
-    bigServo.write(pos);
+/* Moves the small servo a specified position
+@param int degree, the degree of the position to move the small servo 
+ */
+void Claw::writeSmall(int degree){
+  pwm_start(SMALLSERVO_PIN, SERVO_FREQ, 0, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+}
+
+/*Moves the big servo a specified position
+@param int degree, the degree of the position to move the big servo 
+ */
+void Claw::writeBig(int degree){
+  pwm_start(BIGSERVO_PIN, SERVO_FREQ, 0, TimerCompareFormat_t::MICROSEC_COMPARE_FORMAT);
+}
+
+/* Converts the input degree into the pulse width in microseconds to use in pwm_start for the small servo
+@param int degree, the degree to be converted
+ */
+int Claw::convertSmall(int degree){
+  if (degree <= 0) {
+    return MIN_ANGLE_PULSE;
   }
+  if (degree >= MAXANGLE_SMALL) {
+    return MAX_ANGLE_PULSE;
+  }
+  return degree*(MAX_ANGLE_PULSE - MIN_ANGLE_PULSE)/MAXANGLE_SMALL+MIN_ANGLE_PULSE;
+}
+
+/* Converts the input degree into the pulse width in microseconds to use in pwm_start for the big servo
+@param int degree, the degree to be converted
+ */
+int Claw::convertBig(int degree){
+  if (degree <= 0) {
+    return MIN_ANGLE_PULSE;
+  }
+  if (degree >= MAXANGLE_BIG) {
+    return MAX_ANGLE_PULSE;
+  }
+  return degree*(MAX_ANGLE_PULSE - MIN_ANGLE_PULSE)/MAXANGLE_BIG+MIN_ANGLE_PULSE;
+}
